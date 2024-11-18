0465b5030ed7089f2b0f0eec44c7c1f76dff2001
Andy Tran <andy.tran@hotmail.se>
2024-11-18 15:37:31 +0100

    JQB-6: added bare bones client GUI

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,9 +1,28 @@
 package Client;
 
+import java.io.IOException;
+import java.net.InetAddress;
+
 public class Client implements Runnable {
+
+
+    ClientGUI gui;
+    int port;
+    InetAddress ip;
+
+
+    public Client(int port) {
+        this.port = port;
+        ip = InetAddress.getLoopbackAddress();
+    }
+
+    private void connectToServer() {
+        gui.setLabelText("Trying connecting to server.");
+    }
 
     @Override
     public void run() {
-        ClientGUI gui = new ClientGUI();
+         gui = new ClientGUI();
+         connectToServer();
     }
 }
