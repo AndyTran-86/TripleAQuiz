977c2244198146bfdcbc3e435ae6b3a73d80af9e
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-11-18 21:02:32 +0100

    JQB-9 added initial gui for lobby page

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -16,13 +16,11 @@ public class Client implements Runnable {
         ip = InetAddress.getLoopbackAddress();
     }
 
-    private void connectToServer() {
-        gui.setLabelText("Trying connecting to server.");
-    }
 
     @Override
     public void run() {
          gui = new ClientGUI();
-         connectToServer();
+         gui.init();
+
     }
 }
