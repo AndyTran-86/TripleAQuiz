77961600f8fdc084c4825dc6fb982fbea541b39b
Anton Larsson <antonlarsson.musik@gmail.com>
2024-11-19 10:15:17 +0100

    JQB-5: Established connection Server-Client

--- src/Server/Main.java
+++ src/Server/Main.java
@@ -1,4 +1,12 @@
 package Server;
 
 public class Main {
+    public static void main(String[] args) {
+    startServer();
+}
+
+    static void startServer() {
+        Server server = new Server(5555);
+        new Thread(server).start();
+    }
 }
