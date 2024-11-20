383ab0ea984eada8ce9d340e5c3fb20205e3d00a
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-11-20 11:38:26 +0100

    JQB-14 corrected error in request class
    SurrenderRequest class did not extend Request. Fixed it.

--- src/Requests/SurrenderRequest.java
+++ src/Requests/SurrenderRequest.java
@@ -2,5 +2,5 @@ package Requests;
 
 import java.io.Serializable;
 
-public class SurrenderRequest implements Serializable {
+public class SurrenderRequest extends Request implements Serializable {
 }
