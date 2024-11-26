cb0b4141b783d15821fe13f5b0d723623dd8db30
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-11-26 20:27:57 +0100

    JQB-37 added state check in player joined handling in client to fix the interruption.

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -96,8 +96,9 @@ public class Client implements Runnable {
 
                         case PlayerJoinedResponse playerJoinedResponse -> {
                             state.handlePlayerJoined(playerJoinedResponse);
-                            // TODO find a way to update gui with username only - make playerturn state update gui by reading instance valuables, if opponent username != null. update it.
-                            state.updateGUI();
+                            if (state instanceof ClientOtherPlayerTurnState) {
+                                state.updateGUI();
+                            }
                         }
 
                         case RoundPlayedResponse roundPlayedResponse -> {
